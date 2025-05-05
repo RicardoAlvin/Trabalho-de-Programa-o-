1dca242a970b94a68bc52423103c489657c4bf22
RicardoAlvin <ricardocomcecao@gmail.com>
2025-05-05 17:33:53 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+// Figura 14.20: RadioButtonTest.java
+// Testando RadioButtonFrame.
+import javax. swing. JFrame;
+
+public class main{
+
+    public static void main( String[] args ){
+
+        RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+        radioButtonFrame. setDefaultCloseOperation( JFrame. EXIT_ON_CLOSE );
+        radioButtonFrame. setSize( 300, 100 ); // configura o tamanho do frame
+        radioButtonFrame. setVisible( true ); // exibe o frame
+    } // fim de main
+} // fim da classe RadioButtonTest
